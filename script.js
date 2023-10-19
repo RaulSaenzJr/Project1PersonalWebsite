61af8396f35535a321d21b357816efdb4bf887dc
RaulSaenzJr <raul.saenz2@gmail.com>
2023-10-19 00:16:34 -0500

    debugging toggle menu

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-$('a[href*="#"]').on('click', function(e) {
+$('a[href*="#"]').on('click', function (e) {
     e.preventDefault();
     $('html, body').animate({
         scrollTop: $($(this).attr('href')).offset().top
     }, 500, 'linear');
+});
+$(document).ready(function () {
+    // Initialize Bootstrap's collapse plugin
+    $('.navbar-collapse').collapse('hide');
+
+    // Add an event listener for the toggle button click
+    $('.navbar-toggler').on('click', function () {
+        $('.navbar-collapse').collapse('toggle');
+    });
 });
